521caf2397de0f3855fb3f34ee5c3a624879d9e9
vijaynbec <119669904+vijaynbec@users.noreply.github.com>
2025-03-07 11:58:43 -0500

    Create gitsnow.sql
    try it

---
+++ Snowflake/gitsnow.sql
@@ -0,0 +1,14 @@
+// Create table first
+CREATE OR REPLACE TABLE AWS_DB.PUBLIC.movie_titles (
+  show_id STRING,
+  type STRING,
+  title STRING,
+  director STRING,
+  cast STRING,
+  country STRING,
+  date_added STRING,
+  release_year STRING,
+  rating STRING,
+  duration STRING,
+  listed_in STRING,
+  description STRING );
